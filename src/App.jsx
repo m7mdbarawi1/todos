63ff06f0a649cc680a873fe72276f6ec787feff9
m7mdbarawi1 <m7mdbarawi@gmail.com>
2025-05-08 12:15:22 +0300

    good structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-
+import Navbarc from'./components/navbar'
+import Footerc from'./components/footer'
 function App() {
 
   return (
     <>
+    <Navbarc />
     <h1>Hello world</h1>
+    <Footerc/>
     </>
   )
 }
